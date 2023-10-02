6a36a390b36d0e45980b36694759b7240dc34956
Javier Emanuel Huebra <104619744+javierhuebra@users.noreply.github.com>
2023-10-02 01:39:56 -0300

    agregando los cors

--- src/main/java/com/finnegans/demodario/DemodarioApplication.java
+++ src/main/java/com/finnegans/demodario/DemodarioApplication.java
@@ -2,6 +2,12 @@ package com.finnegans.demodario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class DemodarioApplication {
@@ -10,4 +16,18 @@ public class DemodarioApplication {
 		SpringApplication.run(DemodarioApplication.class, args);
 	}
 
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration(); // Allow anyone and anything access.
+		config.setAllowedOriginPatterns(Arrays.asList("*"));
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("DELETE");
+		source.registerCorsConfiguration("/api/**", config);
+		return new CorsFilter(source);
+	}
 }
